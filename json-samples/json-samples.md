b939307aa925d6f9554cac57817c05e3b35a426e
sawyercole <sawyercoletang@gmail.com>
2023-12-28 17:28:40 -0500

    oops removed node_modules

---
+++ json-samples/json-samples.md
@@ -0,0 +1,38 @@
+# json sample post bodies
+
+### `/submit-item-list`
+
+```json
+{
+    "userID": "stang",
+    "location": null,
+    "dateTime": null,
+    "items": [
+        {
+            "name": "ritz_crackers",
+            "price": 3.99,
+            "quantity": 2,
+            "itemID": "idk-what-this-will-be-1"
+        },
+        {
+            "name": "oreo",
+            "price": 4.98,
+            "quantity": 1,
+            "itemID": "idk-what-this-will-be-2"
+        }
+    ]
+
+}
+```
+
+### BUILDING!!!! `/new-user`
+
+```json
+{
+    "userID":"stang",
+    "email":"tangs50@mcmaster.ca",
+    "password":"password123",
+    "firstName":"Sawyer",
+    "lastName":"Tang"
+}
+```
--- src/app.js
+++ src/app.js
@@ -2,9 +2,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/submit-item-list', (req, res) => {
+    const userData = req.body;
+    console.log('Received data:', userData);
+    
+    // Process the data here
+    // ...
+
+    res.status(200).send('Data received successfully');
+});
+
+app.post('/new-user', (req, res) => {
+    const userData = req.body;
+    console.log('Received data:', userData);
+    
+    // Process the data here
+    // ...
+
+    res.status(200).send('Data received successfully');
+});
+
+app.post('/set-user-location', (req, res) => {
+    const userData = req.body;
+    console.log('Received data:', userData);
+    
+    // Process the data here
+    // ...
+
+    res.status(200).send('Data received successfully');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
