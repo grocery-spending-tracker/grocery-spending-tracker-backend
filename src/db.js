aaf43449dcea6ad45e65ea8f9375e534788b04f6
Sawyer Tang <sawyercoletang@gmail.com>
2024-02-05 17:31:37 -0500

    added ssl var

--- src/db.js
+++ src/db.js
@@ -2,10 +2,11 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: process.env.AZURE_POSTGRESQL_USER,
-        host: process.env.AZURE_POSTGRESQL_HOST,
-        database: process.env.AZURE_POSTGRESQL_DATABASE,
-        password: process.env.AZURE_POSTGRESQL_PASSWORD,
-        port: process.env.AZURE_POSTGRESQL_PORT
+    host: process.env.AZURE_POSTGRESQL_HOST,
+    database: process.env.AZURE_POSTGRESQL_DATABASE,
+    password: process.env.AZURE_POSTGRESQL_PASSWORD,
+    port: process.env.AZURE_POSTGRESQL_PORT
+    ssl: process.env.AZURE_POSTGRESQL_SSL
 });
 
 module.exports = pool;
