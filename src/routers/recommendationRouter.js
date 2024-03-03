2d98a33ac30c6321610d420e222d7ac6ee0bd5ed
Sawyer Tang <sawyercoletang@gmail.com>
2024-03-02 22:08:41 -0500

    added semis

--- src/routers/recommendationRouter.js
+++ src/routers/recommendationRouter.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import * as recommendationController from '../controllers/recommendationController.js';
 
-const router = express.Router()
+const router = express.Router();
 
 router.use((req, res, next) => {
-    console.log('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nReceived for /recommendation/')
-    console.log('Time: ', Date.now())
-    next()
+    console.log('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nReceived for /recommendation/');
+    console.log('Time: ', Date.now());
+    next();
 })
 
-router.get("/", recommendationController.getRecommendations)
-router.get("/lowest", recommendationController.getRecommendationsLowestAvailable)
+router.get("/", recommendationController.getRecommendations);
+router.get("/lowest", recommendationController.getRecommendationsLowestAvailable);
 
 export default router;
