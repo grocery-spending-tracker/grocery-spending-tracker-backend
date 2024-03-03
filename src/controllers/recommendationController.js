0b2987c72feca2ec5fe0dc2eed3a68b5ceaf9154
jason-nam <namy2@mcmaster.ca>
2024-03-02 20:29:03 -0500

    separated controller into one for lowest price and one for just frequent purchase

--- src/controllers/recommendationController.js
+++ src/controllers/recommendationController.js
@@ -10,10 +10,41 @@ async function getFrequentlyPurchasedItems(userId) {
     try {
         const client = await pool.connect();
         const query = `
-            SELECT ci.item_key, ci.item_name, ci.price, ci.image_url, t.location, t.date_time, count(*) AS frequency, MIN(ci.price) OVER (PARTITION BY ci.item_key) AS lowest_price
+            SELECT ci.item_key, ci.item_name, ci.price, ci.image_url, t.location, t.date_time, count(*) AS frequency
             FROM classifiedItems ci
             JOIN trips t ON ci.trip_id = t.trip_id
             WHERE t.user_id = $1
+            GROUP BY ci.item_key, ci.item_name, ci.price, ci.image_url, t.location, t.date_time
+            ORDER BY frequency DESC, ci.price ASC
+            LIMIT 10;
+        `;
+        const result = await client.query(query, [userId]);
+        client.release();
+    
+        if (!result.rows.length) {
+            return [];
+        }
+    
+        return result.rows;
+    } catch (error) {
+        console.error('Error fetching frequently purchased items:', error);
+        return [];
+    }
+}
+
+/**
+ * Function to get frequently purchased items that has a lower price comaprison for a user
+ * @param {number} userId
+ * @returns {Promise<Array<object>>}
+ */
+async function getLowestPriceFrequentlyPurchasedItems(userId) {
+    try {
+        const client = await pool.connect();
+        const query = `
+            SELECT ci.item_key, ci.item_name, ci.price, ci.image_url, t.location, t.date_time, count(*) AS frequency, MIN(ci.price) OVER (PARTITION BY ci.item_key) AS lowest_price
+            FROM classifiedItems ci
+            JOIN trips t ON ci.trip_id = t.trip_id
+            WHERE t.user_id = $1 AND (ci.price - lowest_price) >= 0
             GROUP BY ci.item_key, ci.item_name, ci.price, ci.image_url, t.location, t.date_time
             ORDER BY frequency DESC, lowest_price ASC
             LIMIT 10;
@@ -40,11 +71,11 @@ async function getPopularItems() {
     try {
         const client = await pool.connect();
         const query = `
-            SELECT ci.item_key, ci.item_name, ci.image_url, t.location, t.date_time, count(*) AS frequency, MIN(ci.price) OVER (PARTITION BY ci.item_key) AS lowest_price
+            SELECT ci.item_key, ci.item_name, ci.image_url, t.location, t.date_time, count(*) AS frequency
             FROM classifiedItems ci
-            JOIN trips t ON ci.trip_id = t.trip_id
+            JOIN trips t ON ci.trip_id = t.trip_id 
             GROUP BY ci.item_key, ci.item_name, ci.price, ci.image_url, t.location, t.date_time
-            ORDER BY frequency DESC, lowest_price ASC
+            ORDER BY frequency DESC, ci.price ASC
             LIMIT 10;
         `;
         const result = await client.query(query);
@@ -67,6 +98,17 @@ async function recommendItems(userId) {
         const frequentlyPurchasedFromAllUsers = await getPopularItems();
         return frequentlyPurchasedFromAllUsers.slice(0, 3);
     }
+
+    return frequentlyPurchased.slice(0, 3);
+}
+
+/**
+ * Function to recommend items based on lowest price and frequently purchased items
+ * @param {number} userId
+ * @returns {Promise<Array<object>>}
+ */
+async function recommendLowestPriceItems(userId) {
+    const frequentlyPurchased = await getLowestPriceFrequentlyPurchasedItems(userId);
 
     return frequentlyPurchased.slice(0, 3);
 }
@@ -94,6 +136,30 @@ const getRecommendations = async (req, res) => {
     }
 };
 
+const getRecommendationsLowestAvailable = async (req, res) => {
+    try {
+        const callingUser = await Auth.authenticateRequest(req, res);
+        if (callingUser < 0) return;
+
+        console.log("received request for get recommendation from ", callingUser);
+
+        // call singleRecommendation
+        const recommendations = await recommendLowestPriceItems(callingUser); //userid
+
+        // console.log("not implemented yet :(");
+        // res.status(200).json({"message":"not implemented yet :("});
+
+        // let recommendation = {};
+        
+        console.log('Query result:', recommendations);
+        res.status(200).json(recommendations);
+    } catch (err) {
+        console.error('Error executing query', err);
+        res.status(500).send('Database Error ' + err);
+    }
+};
+
 export {
+    getRecommendationsLowestAvailable,
     getRecommendations
 };
